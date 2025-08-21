718a1a74b14a234cef9f303f2af21437b05b2d41
joaopedroluz00 <joaopedroluz600@gmail.com>
2025-08-21 10:40:51 -0300

    build: publish dist to gh-pages

--- Dashboard_campanha_lideres_08.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // 👇 coloque o nome do repositório entre as barras
-  base: "/Lideres-dashboard/", 
-});
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Dashboard_campanha_lideres_08/', // <-- NOME DO REPO
-  plugins: [react()],
-})
